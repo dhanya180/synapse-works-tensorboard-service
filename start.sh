b35658b54a8408568e50e0a001a0b980a017b086
dhanya180 <cs23b055@iittp.ac.in>
2025-07-09 10:46:49 +0000

    hosting tensorboard

---
+++ start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Install Python dependencies
+pip install --no-cache-dir -r requirements.txt
+
+# Start TensorBoard on Render's public port (10000)
+tensorboard --logdir ./tbsummary --host 0.0.0.0 --port 10000
