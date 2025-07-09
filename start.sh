92b412b5a9eff6b0f443bd77e5e6c5a29dbcb5cb
dhanya180 <cs23b055@iittp.ac.in>
2025-07-09 11:04:35 +0000

    changed requirements.txt

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@
 pip install --no-cache-dir -r requirements.txt
 
 # Start TensorBoard on Render's public port (10000)
-tensorboard --logdir ./tbsummary --host 0.0.0.0 --port 10000
+tensorboard --logdir ./tbsummary --host 0.0.0.0 --port 6006
